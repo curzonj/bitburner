676052211b390d421b2811816c94e0812f8b6448
Jordan Curzon <11769+curzonj@users.noreply.github.com>
2023-11-25 05:02:05 +0000

    scan-targets formatting

--- bb/scan-targets.js
+++ bb/scan-targets.js
@@ -3,16 +3,18 @@ import { allServers } from 'bb/lib.js'
 export async function main(ns) {
   const list = allServers(ns);
   const myLevel = ns.getHackingLevel();
-  const format = "%(name)' -20s %(levelReq)' 4s %(money)' 10s %(max)' 10s%(hackDifficulty)' 10s%(minDifficulty)' 6s";
 
-  ns.tprint(ns.sprintf(format, {
-    name: "Name",
-    levelReq: "Level",
-    money: "Avail",
-    max: "Max",
-    hackDifficulty:"Current",
-    minDifficulty:"Min"
-  }))
+  ns.tprint(ns.sprintf(
+    "%(name)' -20s %(levelReq)' 4s %(money)' 10s %(max)' 10s%(hackDifficulty)' 10s%(minDifficulty)' 6s",
+    {
+      name: "Name",
+      levelReq: "Level",
+      money: "Avail",
+      max: "Max",
+      hackDifficulty:"Current",
+      minDifficulty:"Min"
+    }
+  ))
 
   for (var i in list) {
     const name = list[i];
@@ -26,7 +28,8 @@ export async function main(ns) {
     const hackDifficulty = ns.getServerSecurityLevel(name);
     const minDifficulty = ns.getServerMinSecurityLevel(name);
 
-    ns.tprint(ns.sprintf(format,
+    ns.tprint(ns.sprintf(
+      "%(name)' -20s %(levelReq)' 4.0d %(money)' 10s %(max)' 10s%(hackDifficulty)' 10.1d%(minDifficulty)' 6.1d",
       { name, levelReq, money: ns.formatNumber(money), max: ns.formatNumber(maxMoney), hackDifficulty, minDifficulty }
     ));
   }
