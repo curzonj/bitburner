717abf81d5db0a8e110cb96898e0b4a38c1a2755
Jordan Curzon <11769+curzonj@users.noreply.github.com>
2023-11-25 00:54:50 +0000

    add scan-targets.js

---
+++ bb/scan-targets.js
@@ -0,0 +1,30 @@
+/** @param {NS} ns */
+import { allServers } from 'lib.js'
+
+export async function main(ns) {
+  const list = allServers(ns);
+  const myLevel = ns.getHackingLevel();
+  const format = "%(name)' -20s %(levelReq)' 4s %(money)' 10s %(max)' 10s";
+
+  ns.tprint(ns.sprintf(format, {
+    name: "Name",
+    levelReq: "Level",
+    money: "Avail",
+    max: "Max",
+  }))
+
+  for (var i in list) {
+    const name = list[i];
+    if (name == "home") continue;
+
+    const levelReq = ns.getServerRequiredHackingLevel(name);
+    if (levelReq > myLevel / 2) continue;
+
+    const maxMoney = ns.getServerMaxMoney(name);
+    const money = ns.getServerMoneyAvailable(name);
+
+    ns.tprint(ns.sprintf(format,
+      { name, levelReq, money: ns.formatNumber(money), max: ns.formatNumber(maxMoney) }
+    ));
+  }
+}
